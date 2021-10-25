9477a500cbb8e6b1e2f634cbac399568b0bc9300
ducpm151061 <duc.pm151061@sis.hust.edu.vn>
2021-10-25 21:38:52 +0700

    init project

---
+++ components/lvgl_esp32_drivers/lvgl_touch/touch_driver.c
@@ -0,0 +1,55 @@
+/**
+ * @file touch_driver.c
+ */
+
+#include "touch_driver.h"
+#include "tp_spi.h"
+#include "tp_i2c.h"
+
+
+void touch_driver_init(void)
+{
+#if defined (CONFIG_LV_TOUCH_CONTROLLER_XPT2046)
+    xpt2046_init();
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_FT6X06)
+    ft6x06_init(FT6236_I2C_SLAVE_ADDR);
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_STMPE610)
+    stmpe610_init();
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_ADCRAW)
+    adcraw_init();
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_FT81X)
+    /* nothing to do */
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_RA8875)
+    ra8875_touch_init();
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_NS2016)
+    ns2016_init(NS2016_I2C_SLAVE_ADDR);
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_GT615)
+    gt615_init(GT615_I2C_SLAVE_ADDR);
+#endif
+}
+
+bool touch_driver_read(lv_indev_drv_t *drv, lv_indev_data_t *data)
+{
+    bool res = false;
+
+#if defined (CONFIG_LV_TOUCH_CONTROLLER_XPT2046)
+    res = xpt2046_read(drv, data);
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_FT6X06)
+    res = ft6x36_read(drv, data);
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_STMPE610)
+    res = stmpe610_read(drv, data);
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_ADCRAW)
+    res = adcraw_read(drv, data);
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_FT81X)
+    res = FT81x_read(drv, data);
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_RA8875)
+    res = ra8875_touch_read(drv, data);
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_NS2016)
+    res = ns2016_read(drv, data);
+#elif defined (CONFIG_LV_TOUCH_CONTROLLER_GT615)
+    res = gt615_read(drv, data);
+#endif
+
+    return res;
+}
+
